76f9aa680f7f1deefd16861098e61cd71a586b06
VladislavEkimtcov <40220989+VladislavEkimtcov@users.noreply.github.com>
2020-04-21 09:50:07 -0500

    added via upload
    first version of the scripts

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AWSBigRedButton
-AWS incident response rapid operations halt script
+Set of scripts to rapidly suspend AWS activities
+
+Documentation: https://docs.google.com/document/d/1-uugzBGf24aNEdYzWA7ao5yvHPHl82xCjnCxvGtNBsE/edit?usp=sharing
---
+++ depriv.sh
@@ -0,0 +1,3 @@
+aws iam detach-role-policy --role-name scimma_test_power_user --policy-arn arn:aws:iam::585193511743:policy/ProposedPoweruser
+aws iam attach-role-policy --role-name scimma_test_power_user --policy-arn arn:aws:iam::aws:policy/ReadOnlyAccess
+
