5f20694072efc908e8b3a1283fcec64c590693bb
Vladislav Ekimtcov <ekimtcov.v@gmail.com>
2020-08-17 08:03:40 -0500

    CHANGES: defaults.cfg * updated config and added new lines vault.py: * added shlog, for now * added trailscraper wrapper * fixed config parser not retrieving the verbosity level * minor spelling fixes

--- information/bin/vault.py
+++ information/bin/vault.py
@@ -1,26 +1,54 @@
 #!/usr/bin/env python
 """
-Update (or create) a vault directory populated withfiles
-from cloudtrail logs.  
+Update (or create) a vault directory populated with files
+from Cloudtrail logs.
 
-The command uses the shell "aws s3 sync", which provides
-for incremental updates of the files,. The command is
-snesitive to a config file. 
+The command uses the shell "trailscraper", which provides
+for incremental updates of the files. The command is
+sensitive to a config file.
 
 Options available via <command> --help
 """
 import logging
-import subprocess       
+import subprocess
+import information.lib.shlog as shlog
 
 def main(args):
    """
-   Download cloudtrail logs to the vault directory. 
+   Download Cloudtrail logs to the vault directory.
 
    A vault file is a dictionary of "Records" containing an array
    of json objects.  A vault is a directory tree of some depth.
    """
-   cmd = "aws s3 sync {}  {}  --profile {}"
-   cmd = cmd.format(args.bucket, args.vaultdir, args.profile)
+   # retrieve regions
+   import regex as re
+   import boto3
+   args.session = boto3.Session(profile_name=args.profile)
+   regions = au.decribe_regions_df(args)
+
+   # translate security scripts arguments into trailscraper arguments
+   # generate regions argument
+   region_arg = ''
+   for region in regions['RegionName']:
+       region_arg += ' --region ' + region
+    # translate verbosity level
+   if args.loglevel == 'DEBUG':
+       verbosity = '--verbose '
+   else:
+       verbosity = ''
+   # regex the bucket and prefix
+   try:
+       ts_bucket = re.findall('(?<=s3:\/\/)(.*?)(?=\/)', args.bucket)[0]
+       ts_prefix = re.findall('([^\/]+$)', args.bucket)[0]
+   except IndexError:
+       shlog.normal('error parsing bucket ' + args.bucket + ', s3://bucket/prefix format is expected')
+       exit(0)
+
+   cmd = "trailscraper " + verbosity + "download " \
+         "--bucket " + ts_bucket + " " \
+         "--prefix " + ts_prefix + "/ " \
+         "--account-id " + args.accountid + " " \
+         + region_arg
    logging.info(cmd)
    status = subprocess.run(cmd, shell=True)
    exit(status.returncode)
@@ -29,23 +57,25 @@ if __name__ == "__main__":
 
    import argparse 
    import configparser
+   import information.lib.aws_utils as au
 
    config = configparser.ConfigParser()
    config.read_file(open('defaults.cfg'))
-   vaultdir = config.get("DEFAULT", "vault directory", fallback="./vault")
-   profile  = config.get("DEFAULT", "profile", fallback="sciimma-aws-admin")
-   loglevel = config.get("DEFAULT", "loglevel",fallback="INFO")
-   bucket   = config.get("DOWNLOAD", "bucket",fallback="s3://scimma-processes/Scimma-event-trail")
+   vaultdir = config.get("DEFAULT", "vault directory", fallback="~/.trailscraper")
+   profile  = config.get("DEFAULT", "profile", fallback="default")
+   accountid = config.get("DOWNLOAD", "accountid",fallback="585193511743")
+   loglevel = config.get("DOWNLOAD", "loglevel",fallback="INFO")
+   bucket   = config.get("DOWNLOAD", "bucket")
    
    
    """Create command line arguments"""
    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-   parser.add_argument('--profile','-p',default=profile,
-             help='aws profile to use')
+   parser.add_argument('--profile','-p',default=profile,help='aws profile to use')
    parser.add_argument('--debug'   ,'-d',help='print debug info', default=False, action='store_true')
-   parser.add_argument('--loglevel','-l',help="Level for  reporning e.r DEBUG, INFO, WARN", default="INFO")
+   parser.add_argument('--loglevel','-l',help="Level for reporting e.g. DEBUG, INFO, WARN", default=loglevel)
    parser.add_argument('--vaultdir'     ,help='vault directory def:%s' % vaultdir, default=vaultdir)
    parser.add_argument('--bucket'       ,help='bucket with cloudtail logs', default=bucket)
+   parser.add_argument('--accountid', help='AWS account id', default=accountid)
 
    args = parser.parse_args()
    logging.basicConfig(level=args.loglevel)
