7d030ae05886bedf75c79ecaf8e9a5de66d7efea
Vladislav Ekimtcov <ekimtcov.v@gmail.com>
2020-09-02 12:19:15 -0500

    repo restructuting, first working cli interface

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="security-scripts", # Replace with your own username
+    version="0.9b",
+    author="Vladislav Ekimtcov, Donald Petravic",
+    author_email="ekimtco2@illinois.edu",
+    description="SCiMMA security scripts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scimma/security-scripts",
+    packages=setuptools.find_packages(),
+    package_data={
+        "": ["*.cfg"] # carry what is yours
+    },
+    entry_points={
+        'console_scripts': ['sc=security_scripts.cli:catcher'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: none",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
