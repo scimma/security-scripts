49c47aea55e7e2c2d0b2f1d240cb74c62d3b16c0
dpetravick <dpetravick@yahoo.com>
2021-05-14 13:09:54 -0500

    a command to make reules in a uniform way, according to our conventions

---
+++ security_scripts/rdk/rdk_new_rule.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+"""
+use the rdk command to make a new rule, using the
+options we have decied is our coding conventions.
+
+examples:
+
+#look for multiple patters for CI's 
+rdk sample-ci sas 2>&1 | tr , "\n" | grep -i 'sub\|s3'
+
+Options available via <command> --help
+
+"""
+#import shlog
+import sys
+import os
+import re
+
+def execute_or_not(args, cmd):
+    if not args.dry_run:
+        print("executing:",cmd)
+        status = os.system(cmd)
+    else:
+        print ("would have executed: \n {}".format(cmd))
+
+def main(args):
+   "perform (or dry run) the rdk create"
+
+   # speify the default taglist in plain python.
+   # then reformat to be shell appropos.
+   default_json_tag_list = '''
+   [
+   {"Key" : "Criticality","Value" : "Development"}.
+   {"Key" : "Service"    ,"Value" : "OpSec"}
+   ]
+   '''
+   default_json_tag_list = default_json_tag_list.replace("\n","")
+   default_json_tag_list = default_json_tag_list.strip()
+   default_json_tag_list = re.sub('\s+',' ',default_json_tag_list)
+   default_json_tag_list = "'{}'".format(default_json_tag_list)
+
+   cmd = '''rdk sample-ci sas 2>&1 | tr , "\\n" | grep -i '{}' '''.format(args.cipattern)
+   execute_or_not(args, cmd)
+   
+   cmd = "rdk create {}  --tags {} ".format(args.rulename, default_json_tag_list)
+   execute_or_not(args, cmd)
+          
+if __name__ == "__main__":
+
+   import argparse 
+   import configparser
+   
+
+   """ get defaults from configuration system"""
+   #from security_scripts.kli import env_control
+   config = configparser.ConfigParser()
+   import os
+   #rel_path = "../information/defaults.cfg"
+   #cfg_sources = [rel_path,  # built-in config for fallback
+   #               os.path.expanduser(env_control())  # env value
+   #               ]
+   #config.read(cfg_sources)
+
+   #profile  = config.get("TAG_REPORT", "profile")
+   #loglevel = config.get("TAG_REPORT", "loglevel",fallback="NORMAL")
+   loglevel="NORMAL"
+   profile = "scimma-uiuc-aws-admin"
+
+   """Create command line arguments"""
+   parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+   parser.add_argument('--loglevel', '-l', help="Level for reporting e.g. NORMAL, VERBOSE, DEBUG (default: %(default)s)",
+                       default=loglevel,
+                       choices=["NONE", "NORMAL", "DOTS", "WARN", "ERROR", "VERBOSE", "VVERBOSE", "DEBUG"])
+   parser.add_argument('--dry-run','-n',default=False, action='store_true',
+                       help = "dry run just show what would be done")
+   
+   parser.add_argument('rulename',
+                    help='name of rule to create')
+   parser.add_argument('cipattern',
+                    help='grep friendly pattern to show CIs')
+   #parser = parser_builder(None, parser, config, False)
+
+   args = parser.parse_args()
+   print (args)
+   main(args)
+
